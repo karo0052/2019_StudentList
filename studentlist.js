f75c1e9fcee39a1847549890d52260469d2fdbd7
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2019-02-10 22:32:47 +0100

    modalvindue

--- studentlist.js
+++ studentlist.js
@@ -3,8 +3,5 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
-  console.log("init");
-
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
-
 }
